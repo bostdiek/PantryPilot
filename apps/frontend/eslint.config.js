40a6f51a8157bc93936976e1af4e2c144abf171a
Bryan Ostdiek <bostdiek@gmail.com>
2025-07-26 20:41:47 -0400

    Update frontend development tooling and fix ESLint configuration

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -1,12 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 // Removed invalid import of globalIgnores
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  {
+    ignores: ['dist'],
+  },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -20,4 +22,4 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
-])
+]);
