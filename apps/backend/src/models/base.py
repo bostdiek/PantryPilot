487b4cc660afa5b410aa1d4a50ccd9b7fae44736
kostdiek <kmostdiek@gmail.com>
2025-08-21 16:06:00 -0400

    stuff!

---
+++ apps/backend/src/models/base.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData
+
+# Create a metadata object with naming convention for constraints
+metadata = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+)
+
+# Create the declarative base class
+Base = declarative_base(metadata=metadata) 
