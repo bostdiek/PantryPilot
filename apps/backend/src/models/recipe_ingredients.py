d142fcfa70f9af8834cdafd936770927c5de01ac
Karen Ostdiek <kmostdiek@gmail.com>
2025-08-22 15:47:43 -0400

    still too long

--- apps/backend/src/models/recipe_ingredients.py
+++ apps/backend/src/models/recipe_ingredients.py
@@ -7,7 +7,6 @@ from sqlalchemy import (
     Column,
     DateTime,
     ForeignKey,
-    Integer,
     String,
     Text,
 )
@@ -20,8 +19,9 @@ class RecipeIngredient(Base):
     __tablename__ = "recipe_ingredients"
 
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
-    recipe_id = Column(Integer, ForeignKey("recipe_names.id"), nullable=False)
-    ingredient_id = Column(Integer, ForeignKey("ingredient_names.id"), nullable=False)
+    recipe_id=Column(UUID(as_uuid=True), ForeignKey("recipe_names.id"), nullable=False)
+    ingredient_id=Column(UUID(as_uuid=True), ForeignKey("ingredient_names.id")
+        , nullable=False)
     quantity = Column(String(255), nullable=False)
     unit = Column(String(255), nullable=False)
     is_optional = Column(Boolean, default=False)
