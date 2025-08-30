1ced8bff24667162b481a0b6863cf9f2c77098ab
Bryan Ostdiek <bostdiek@gmail.com>
2025-08-30 12:43:39 -0400

    refactor: Remove unnecessary virtual option from mock configuration in mockIcons.tsx

--- apps/frontend/src/components/ui/__tests__/mockIcons.tsx
+++ apps/frontend/src/components/ui/__tests__/mockIcons.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 import { vi } from 'vitest';
 
 // Mock SVGs for tests
-vi.mock(
-  '../icons/check.svg?react',
-  () => ({
-    default: (props: React.SVGProps<SVGSVGElement>) =>
-      React.createElement('svg', { ...props, 'data-testid': 'check-icon' }),
-  }),
-  { virtual: true }
-);
+vi.mock('../icons/check.svg?react', () => ({
+  default: (props: React.SVGProps<SVGSVGElement>) =>
+    React.createElement('svg', { ...props, 'data-testid': 'check-icon' }),
+}));
 
 // Re-export the mocked icon
 export { default as CheckIcon } from '../icons/check.svg?react';
